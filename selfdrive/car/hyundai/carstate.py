be4a482fcbb42cb29426038535928e16bba66560
TerminatorNL <terminatornl@users.noreply.github.com>
2024-06-07 19:33:04 +0200

    Better debugging tools would be nice.

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -289,9 +289,6 @@ class CarState(CarStateBase):
           ("SCC12", 50),
         ]
 
-    if CP.flags & HyundaiFlags.USE_FCA.value:
-      messages.append(("FCA11", 50))
-
     if CP.enableBsm:
       messages.append(("LCA11", 50))
 
